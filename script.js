6a86a843a05a21c417e3c28736c70d32773155cc
darshubutani <116871718+darshubutani@users.noreply.github.com>
2022-11-30 12:56:16 +0530

    added: index, script, style

---
+++ script.js
@@ -0,0 +1,147 @@
+'use strict'
+/////
+const InputNameS = document.getElementById('signupName');
+const InputEmailS = document.getElementById('signupEid');
+const InputPswdS = document.getElementById('signupPswd');
+const signupBtn = document.querySelector('.submit-btn');
+const btnlogin = document.getElementById('btnLogin');
+const InputEmailL = document.getElementById('LoginEid');
+const InputPswdL = document.getElementById('LoginPswd');
+const containerApp = document.querySelector('.form-structor');
+const appContainer = document.querySelector('.app');
+appContainer.style.opacity = 0;
+////
+
+let form = document.getElementById("form");
+let nameInput = document.getElementById("nameInput");
+let emailInput = document.getElementById("emailInput");
+let pswdInput = document.getElementById("pswdInput");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+let btnlogout = document.getElementById("logout");
+let data = [{}];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (nameInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
+};
+
+
+let acceptData = () => {
+  data.push({
+    names: nameInput.value || InputNameS.value,
+    email: emailInput.value || InputEmailS.value,
+    password: pswdInput.value || InputPswdS.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+  createTasks();
+};
+
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, index) => {
+    return (tasks.innerHTML += `
+    <div id=${index}>
+          <span class="fw-bold">${x.names}</span>
+          <span class="small text-secondary">${x.email}</span>
+          <p>${x.password}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
+
+  resetForm();
+};
+
+let deleteTask = (e) => {
+  confirm('Are you sure to delete this record ?')
+  //e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+
+};
+
+let editTask = (e) => {
+
+  let selectedTask = e.parentElement.parentElement;
+  nameInput.value = selectedTask.children[0].innerHTML;
+  emailInput.value = selectedTask.children[1].innerHTML;
+  pswdInput.value = selectedTask.children[2].innerHTML;
+  window.confirm = function () { return false; };
+  deleteTask(e);
+};
+
+let resetForm = () => {
+  nameInput.value = "";
+  emailInput.value = "";
+  pswdInput.value = "";
+};
+//IIFY to display data 
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || []
+  console.log(data);
+
+  createTasks();
+})();
+
+
+//////
+signupBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log("Hello signup");
+  containerApp.style.opacity = 0;
+  appContainer.style.opacity = 100;
+  acceptData();
+});
+
+let temp;
+btnlogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (data.find(acc => acc.email === InputEmailL.value)) {
+    temp = data.find(acc => acc.email === InputEmailL.value);
+    console.log(temp);
+    if ((temp.password) === (InputPswdL.value)) {
+      console.log('LogIN');
+      containerApp.style.opacity = 0;
+      appContainer.style.opacity = 100;
+    }
+    else {
+      alert("Incorrect password")
+    }
+  }
+  else {
+    alert("Account not found");
+  }
+});
+
+btnlogout.addEventListener('click',function(e){
+  e.preventDefault();
+     containerApp.style.opacity = 100;
+     appContainer.style.opacity = 0;
+});
+/////
