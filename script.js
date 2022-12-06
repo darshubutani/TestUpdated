c6280aa4070e80aafab5a750e218e455ea348bd9
darshubutani <116871718+darshubutani@users.noreply.github.com>
2022-12-06 18:09:41 +0530

    Feat: Data for perticular user

--- script.js
+++ script.js
@@ -25,51 +25,61 @@ let add = document.getElementById("add");
 let btnlogout = document.getElementById("logout");
 let btnLogin2 = document.getElementById("btnLogin2");
 //let login;
-let data = [{}];
+let currentUser;
+//let data = [{}];
 
+let Darshna = {
+    name: 'Darshna',
+    password: 123,
+    data: [{}],
+}
+
+let users = [Darshna];
+
+//localStorage.setItem("users",JSON.stringify(users));
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  formValidation();
+    e.preventDefault();
+    formValidation();
 });
 
 let formValidation = () => {
-  if (nameInput.value === "") {
-    console.log("failure");
-    msg.innerHTML = "Task cannot be blank";
-  } else {
-    console.log("success");
-    msg.innerHTML = "";
-    acceptData();
-    add.setAttribute("data-bs-dismiss", "modal");
-    add.click();
+    if (nameInput.value === "") {
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
 
-    (() => {
-      add.setAttribute("data-bs-dismiss", "");
-    })();
-  }
+        (() => {
+            add.setAttribute("data-bs-dismiss", "");
+        })();
+    }
 };
 
-
 let acceptData = () => {
-  data.push({
-    names: nameInput.value || InputNameS.value,
-    email: emailInput.value || InputEmailS.value,
-    password: pswdInput.value || InputPswdS.value,
-  });
+    currentUser.data.push({
+        names: nameInput.value,
+        email: emailInput.value,
+        password: pswdInput.value,
+    });
 
-  localStorage.setItem("data", JSON.stringify(data));
-  console.log(data);
-  createTasks();
+    localStorage.setItem(`${currentUser.name}`, JSON.stringify(currentUser.data));
+    console.log(currentUser.data);
+    createTasks();
 };
 
 let createTasks = () => {
-  tasks.innerHTML = "";
-  data.map((x, index) => {
-    return (tasks.innerHTML += `
+    tasks.innerHTML = "";
+    currentUser.data.filter(acc => acc.names !== undefined && acc.names !== '')
+        .map((x, index) => {
+            return (tasks.innerHTML += `
     <div id=${index}>
-          <span class="fw-bold">${x.names}</span>
-          <span class="small text-secondary">${x.email}</span>
+          <p>${x.names}</p>
+          <p>${x.email}</p>
           <p>${x.password}</p>
   
           <span class="options">
@@ -78,118 +88,126 @@ let createTasks = () => {
           </span>
         </div>
     `);
-  });
 
-  resetForm();
+        });
+
+    console.log("Inside login" + currentUser.data);
+    resetForm();
 };
 
 let deleteTask = (e) => {
-  confirm('Are you sure to delete this record ?')
-  //e.parentElement.parentElement.remove();
-  data.splice(e.parentElement.parentElement.id, 1);
-  localStorage.setItem("data", JSON.stringify(data));
-  console.log(data);
+    confirm('Are you sure to delete this record ?')
+    e.parentElement.parentElement.remove();
+    currentUser.data.splice(e.parentElement.parentElement.id, 1);
+    localStorage.setItem(`${currentUser.name}`, JSON.stringify(currentUser.data));
+    console.log(currentUser.data);
 
 };
 
 let editTask = (e) => {
 
-  let selectedTask = e.parentElement.parentElement;
-  nameInput.value = selectedTask.children[0].innerHTML;
-  emailInput.value = selectedTask.children[1].innerHTML;
-  pswdInput.value = selectedTask.children[2].innerHTML;
-  window.confirm = function () { return false; };
-  deleteTask(e);
+    let selectedTask = e.parentElement.parentElement;
+    nameInput.value = selectedTask.children[0].innerHTML;
+    emailInput.value = selectedTask.children[1].innerHTML;
+    pswdInput.value = selectedTask.children[2].innerHTML;
+    window.confirm = function () { return false; };
+    deleteTask(e);
 };
 
 let resetForm = () => {
-  nameInput.value = "";
-  emailInput.value = "";
-  pswdInput.value = "";
-  InputNameS.value = "";
-  InputEmailS.value = "";
-  InputPswdS.value = "";
-  InputPswdL.value = "";
-  InputEmailL.value = "";
+    nameInput.value = emailInput.value = pswdInput.value = InputNameS.value = InputEmailS.value = InputPswdS.value = InputPswdL.value = InputEmailL.value = "";
+
 };
-//IIFE to display data 
-(() => {
-  data = JSON.parse(localStorage.getItem("data")) || []
-  console.log(data);
-
-  if (localStorage.login == 1) {
-    console.log('already Logged in ');
-    containerApp.style.opacity = 0;
-    containerSignup.style.opacity = 0;
-    containerLogin.style.opacity = 0;
-    appContainer.style.opacity = 100;
-  }
-  else {
-    console.log("Login first");
-  }
-
-
-  createTasks();
-})();
-
 
 //////
 signupBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log("Hello signup");
-  localStorage.login = 1;
-  containerApp.style.opacity = 0;
-  appContainer.style.opacity = 100;
-  containerSignup.style.opacity = 0;
-
-  acceptData();
+    e.preventDefault();
+    console.log("Hello signup");
+    users = JSON.parse(localStorage.getItem("users"));
+    localStorage.login = 1;
+    containerApp.style.opacity = 0;
+    appContainer.style.opacity = 100;
+    containerSignup.style.opacity = 0;
+    let user = (InputNameS.value);
+    console.log(InputNameS.value);
+    users.push(user = {
+        name: (InputNameS.value),
+        password: (InputPswdS.value),
+        data: [{}],
+    });
+    localStorage.setItem("users", JSON.stringify(users));
+    console.log(users);
+    currentUser = user;
+    acceptData();
 });
 
-let temp;
+
 btnlogin.addEventListener('click', function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  console.log(localStorage.login);
+    console.log(localStorage.login);
+    users = JSON.parse(localStorage.getItem("users"));
+    if (users.find(acc => acc.name == InputEmailL.value)) {
+        currentUser = users.find(acc => acc.name == InputEmailL.value);
+        console.log(currentUser);
+        if ((currentUser.password) == (InputPswdL.value)) {
+            console.log('LogIN');
+            localStorage.login = 1;
+            containerApp.style.opacity = 0;
+            containerSignup.style.opacity = 0;
+            containerLogin.style.opacity = 0;
+            appContainer.style.opacity = 100;
 
-  if (data.find(acc => acc.email === InputEmailL.value)) {
-    temp = data.find(acc => acc.email === InputEmailL.value);
-    console.log(temp);
-    if ((temp.password) === (InputPswdL.value)) {
-      console.log('LogIN');
-      localStorage.login = 1;
-      containerApp.style.opacity = 0;
-      containerSignup.style.opacity = 0;
-      containerLogin.style.opacity = 0;
-      appContainer.style.opacity = 100;
+            (() => {
+                currentUser.data = JSON.parse(localStorage.getItem(`${currentUser.name}`)) || []
+                console.log(currentUser.data);
+                createTasks();
+            })();
+        }
+        else {
+            alert("Incorrect password")
+        }
     }
     else {
-      alert("Incorrect password")
+        alert("Account not found");
     }
-  }
-  else {
-    alert("Account not found");
-  }
 });
 
 btnlogout.addEventListener('click', function (e) {
-  e.preventDefault();
-  localStorage.login = 0;
-  containerApp.style.opacity = 100;
-  appContainer.style.opacity = 0;
-  containerLogin.style.opacity = 0;
-  containerSignup.style.opacity = 100;
-  InputNameS.value = "";
-  InputEmailS.value = "";
-  InputPswdS.value = "";
-  InputPswdL.value = "";
-  InputEmailL.value = "";
+    e.preventDefault();
+    localStorage.login = 0;
+    containerApp.style.opacity = 100;
+    appContainer.style.opacity = 0;
+    containerLogin.style.opacity = 0;
+    containerSignup.style.opacity = 100;
+    InputNameS.value = "";
+    InputEmailS.value = "";
+    InputPswdS.value = "";
+    InputPswdL.value = "";
+    InputEmailL.value = "";
 });
 
 btnLogin2.addEventListener('click', function (e) {
-  e.preventDefault();
-  containerApp.style.opacity = 100;
-  appContainer.style.opacity = 0;
-  containerLogin.style.opacity = 100;
-  containerSignup.style.opacity = 0;
+    e.preventDefault();
+    containerApp.style.opacity = 100;
+    appContainer.style.opacity = 0;
+    containerLogin.style.opacity = 100;
+    containerSignup.style.opacity = 0;
 });
 /////
+
+// (() => {
+ 
+//     if (localStorage.login == 1) {
+//       console.log('already Logged in ');
+//       containerApp.style.opacity = 0;
+//       containerSignup.style.opacity = 0;
+//       containerLogin.style.opacity = 0;
+//       appContainer.style.opacity = 100;
+//     }
+//     else {
+//       console.log("Login first");
+//     }
+  
+//     createTasks();
+//   })();
